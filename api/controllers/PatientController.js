d9f87a19aaf25ce81e9e16ddb22d5c25b28ab915
Gab-Metzger <gab.metzger@gmail.com>
2015-03-17 16:26:58 +0100

    Add note in patientController

--- api/controllers/PatientController.js
+++ api/controllers/PatientController.js
@@ -20,6 +20,7 @@ module.exports = {
       mobilePhone: params.mobilePhone,
       phone: params.phone,
       address: params.address,
+      notes: params.notes,
       password: password,
       confirmation: password
     };
