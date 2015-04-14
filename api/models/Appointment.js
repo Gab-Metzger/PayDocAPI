d6e7032a71b719e0fb275f29b83da1ec23ba718c
Gab-Metzger <gab.metzger@gmail.com>
2015-04-14 21:42:45 +0200

    Change appoint color

--- api/models/Appointment.js
+++ api/models/Appointment.js
@@ -42,14 +42,14 @@ module.exports = {
     toJSON: function() {
       var obj = this.toObject();
       switch(obj.state) {
-        case 'pending'   :  obj.color = '#FF9900';  break;
-        case 'approved':  obj.color = 'green';  break;
+        case 'pending'   :  obj.color = '#F4FA58';  break;
+        case 'approved':  obj.color = '#82FA58 ';  break;
         case 'denied'  :  obj.color = 'red';  break;
       }
       if (obj.patient != null) {
         obj.title = obj.patient.name;
         if ((parseInt(obj.patient) !== obj.patient) && (obj.patient.email.indexOf('paydoc.fr') != -1) && (obj.patient.mobilePhone == null)) {
-          obj.color = '#2C6EBE';
+          obj.color = '#2E64FE';
         }
       }
       else {
