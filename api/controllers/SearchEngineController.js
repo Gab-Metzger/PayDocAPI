ed97af606086372d0ba9f3c632c1a7d6fb0bafd2
Gab-Metzger <gab.metzger@gmail.com>
2015-05-17 18:52:22 +0200

    Search controller action search

--- api/controllers/SearchEngineController.js
+++ api/controllers/SearchEngineController.js
@@ -15,16 +15,54 @@ module.exports = {
    * `SearchEngineController.search()`
    */
   search: function (req, res) {
-		var range = rangeToDate(req.param('day'), req.param('period'));
+		var range = rangeToDate(JSON.parse(req.param('days')), JSON.parse(req.param('periods')));
 		var nbrRes = 0;
+    var availableAppointments = [];
+    var interval = req.param('interval') || 15;
+    var count = 0;
+    var isAvailable;
+    var query = {};
 
-		Appointment.find({start: { '>': new Date(range.start), '<': new Date(range.end) }, end: { '>': new Date(range.start), '<': new Date(range.end) }})
-			.exec(function(err, results) {
-				console.log(results);
-			})
-    return res.json({
-      todo: 'search() is not implemented yet!'
-    });
+    async.whilst(
+      function () { return (count < range.length || nbrRes < 5); },
+      function (callback) {
+      isAvailable = true;
+
+      query = {
+        start: {
+          '>=': new Date(range[count].start).toISOString(),
+          '<': new Date(range[count].end).toISOString()
+        },
+        end: {
+          '>=': new Date(range[count].start).toISOString(),
+          '<': new Date(range[count].end).toISOString()
+        }
+      };
+
+      Appointment.find(query)
+        .exec(function(err, results) {
+          console.log(count);
+          while (moment(range[count].start).isBefore(moment(range[count].end)) && (nbrRes <= 4)) {
+            isAvailable = true;
+            for (var j = 0; j < results.length; j++) {
+              if (moment(range[count].start).isSame(moment(results[j].start))) {
+                isAvailable = false;
+              }
+            }
+            if (isAvailable) {
+              availableAppointments.push(moment(range[count].start));
+              nbrRes++;
+            }
+            range[count].start = moment(range[count].start).add(interval,'minutes');
+          }
+        })
+        count++;
+      },
+      function (err) {
+        console.log(nbrRes);
+        return res.json(availableAppointments);
+      }
+    );
   },
 
 };
@@ -32,29 +70,33 @@ module.exports = {
 /**
 * `rangeToDate()`
 */
-function rangeToDate(day, period) {
-	var dayOfWeek = parseInt(day);
-	var interval = period;
-	var start = moment().weekday(dayOfWeek);
-	var end = moment().weekday(dayOfWeek);
+function rangeToDate(days, periods) {
+  var daysOfWeek = days;
+  var intervals = periods;
 
-	switch(interval) {
-		case 'morning':
-			start.set({'hour': 9, 'minute': 0, 'second': 0, 'millisecond': 0});
-			end.set({'hour': 12, 'minute': 0, 'second': 0, 'millisecond': 0});
-		break;
-		case 'afternoon':
-			start.set({'hour': 14, 'minute': 0, 'second': 0, 'millisecond': 0});
-			end.set({'hour': 17, 'minute': 0, 'second': 0, 'millisecond': 0});
-		break;
-		case 'evening':
-			start.set({'hour': 17, 'minute': 0, 'second': 0, 'millisecond': 0});
-			end.set({'hour': 20, 'minute': 0, 'second': 0, 'millisecond': 0});
-		break;
-	}
+  var result = [];
 
-	return {
-		start: start,
-		end: end
-	};
+  for(var i=0; i < intervals.length; i++) {
+    var start = moment().weekday(daysOfWeek[i]);
+    var end = moment().weekday(daysOfWeek[i]);
+
+    switch(intervals[i]) {
+      case 'morning':
+        start.set({'hour': 9, 'minute': 0, 'second': 0, 'millisecond': 0});
+        end.set({'hour': 12, 'minute': 0, 'second': 0, 'millisecond': 0});
+      break;
+      case 'afternoon':
+        start.set({'hour': 14, 'minute': 0, 'second': 0, 'millisecond': 0});
+        end.set({'hour': 17, 'minute': 0, 'second': 0, 'millisecond': 0});
+      break;
+      case 'evening':
+        start.set({'hour': 17, 'minute': 0, 'second': 0, 'millisecond': 0});
+        end.set({'hour': 20, 'minute': 0, 'second': 0, 'millisecond': 0});
+      break;
+    }
+
+    result.push({start: start, end: end});
+  }
+
+  return result;
 }
